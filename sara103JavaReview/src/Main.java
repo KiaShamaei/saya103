83c304a8c16dc4efbc044199ed9deb4c15261b28
kiarash.shamaii <kiarash.shamaii@gmail.com>
2023-09-22 17:29:17 +0330

    f1

---
+++ sara103JavaReview/src/Main.java
@@ -0,0 +1,14 @@
+
+
+
+/**
+ * this is for review java core sara 103
+ * first
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
