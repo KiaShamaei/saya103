7917be2e7cf4a2fe42fd219d063cddea7863e96e
kiarash.shamaii <kiarash.shamaii@gmail.com>
2023-09-22 18:29:13 +0330

    sara java review

---
+++ T1.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class T1 {
+    public static void main(String[] args) {
+
+        System.out.println("hello sara ...");
+        System.out.println(LocalDate.now());
+
+    }
+}
--- sara103JavaReview/src/Main.java
+++ sara103JavaReview/src/Main.java
@@ -6,9 +6,12 @@
  * first
  */
 
-
+import
 public class Main {
+
     public static void main(String[] args) {
+
+        Date
         System.out.println("Hello world!");
     }
 }
