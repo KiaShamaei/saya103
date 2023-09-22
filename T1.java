646f820405d271a65004233affc1f5805d185357
kiarash.shamaii <kiarash.shamaii@gmail.com>
2023-09-22 18:49:44 +0330

    sara java

--- T1.java
+++ T1.java
@@ -2,9 +2,8 @@ import java.time.LocalDate;
 
 public class T1 {
     public static void main(String[] args) {
-
         System.out.println("hello sara ...");
-        System.out.println(LocalDate.now());
 
+        System.out.println(LocalDate.now());
     }
 }
